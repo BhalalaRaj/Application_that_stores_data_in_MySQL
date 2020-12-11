e8c7e715a923273ca8e9c6161f132da753ceff78
Raj Bhalala <rajbhalala.rb@gmail.com>
2020-12-11 20:27:18 +0530

    Data Store complete

--- app/src/main/java/com/example/dell/mysql/Constants.java
+++ app/src/main/java/com/example/dell/mysql/Constants.java
@@ -1,5 +1,7 @@
 package com.example.dell.mysql;
 
 public class Constants {
-    public static final String Root_URL="http://localhost/Android/v1/registerUser.php";
+    private static final String Root_URL="http://192.168.43.169/Android/v1/";
+    public static final String URL_REGISTER = Root_URL+"registerUser.php";
+
 }
--- app/src/main/java/com/example/dell/mysql/MainActivity.java
+++ app/src/main/java/com/example/dell/mysql/MainActivity.java
@@ -1,13 +1,97 @@
 package com.example.dell.mysql;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText Username,Email,Password;
+    private Button Register;
+    private ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Username = findViewById(R.id.Username);
+        Email = findViewById(R.id.Email);
+        Password = findViewById(R.id.Password);
+        Register = findViewById(R.id.Register);
+
+        progressDialog = new ProgressDialog(this);
+        Register.setOnClickListener(this);
+    }
+
+    private void registerUser(){
+        final String getEmail = Email.getText().toString().trim();
+        final String getUsername = Username.getText().toString().trim();
+        final String getPassword = Password.getText().toString().trim();
+
+        progressDialog.setMessage("Registering User");
+        progressDialog.show();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, Constants.URL_REGISTER,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        progressDialog.dismiss();
+
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            Toast.makeText(getApplicationContext(),jsonObject.getString("message"),Toast.LENGTH_LONG).show();
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                progressDialog.hide();
+                Toast.makeText(getApplicationContext(),error.getMessage(),Toast.LENGTH_LONG).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                params.put("username",getUsername);
+                params.put("email",getEmail);
+                params.put("password",getPassword);
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+    @Override
+    public void onClick(View view) {
+        if(view==Register)
+        {
+            registerUser();
+        }
+
     }
 }
