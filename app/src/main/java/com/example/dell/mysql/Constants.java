488dd760920097215ef17a226877fec1f98c6d9c
Raj Bhalala <rajbhalala.rb@gmail.com>
2020-12-15 22:13:49 +0530

    Shared Preference for login

--- app/src/main/java/com/example/dell/mysql/Constants.java
+++ app/src/main/java/com/example/dell/mysql/Constants.java
@@ -3,5 +3,6 @@ package com.example.dell.mysql;
 public class Constants {
     private static final String Root_URL="http://192.168.43.169/Android/v1/";
     public static final String URL_REGISTER = Root_URL+"registerUser.php";
+    public static final String URL_LOGIN = Root_URL+"userLogin.php";
 
 }
--- app/src/main/java/com/example/dell/mysql/LoginActivity.java
+++ app/src/main/java/com/example/dell/mysql/LoginActivity.java
@@ -1,13 +1,103 @@
 package com.example.dell.mysql;
 
+import android.app.ProgressDialog;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText Username,Password;
+    private Button Login;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Username = findViewById(R.id.Username);
+        Password = findViewById(R.id.Password);
+        Login = findViewById(R.id.Login);
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("please wait");
+        Login.setOnClickListener(this);
+    }
+
+    private void userLogin(){
+        final String getUsername = Username.getText().toString().trim();
+        final String getPassword = Password.getText().toString().trim();
+
+        progressDialog.show();
+
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.POST,
+                Constants.URL_LOGIN,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        progressDialog.dismiss();
+
+                        try {
+                            JSONObject obj = new JSONObject(response);
+                            if(!obj.getBoolean("error"))
+                            {
+                                SharedPrefManager.getInstance(getApplicationContext())
+                                        .userLogin(obj.getInt("id"),
+                                                obj.getString("username"),
+                                                obj.getString("email"));
+                                Toast.makeText(getApplicationContext(),"user login successful",Toast.LENGTH_LONG).show();
+                            }else{
+                                Toast.makeText(getApplicationContext(),obj.getString("message"),Toast.LENGTH_LONG).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        progressDialog.dismiss();
+                        Toast.makeText(getApplicationContext(),error.getMessage(),Toast.LENGTH_LONG).show();
+                    }
+                }
+        ){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                params.put("username",getUsername);
+                params.put("password",getPassword);
+                return params;
+            }
+        };
+        RequestHandler.getInstance(this).addToRequestQueue(stringRequest);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view==Login)
+        {
+            userLogin();
+        }
     }
 }
