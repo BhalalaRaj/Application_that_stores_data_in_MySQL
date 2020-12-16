3df5aeb007f347d8963ee5c8814080e316d2f99c
Raj Bhalala <rajbhalala.rb@gmail.com>
2020-12-16 11:31:43 +0530

    Complete CRED operations

--- app/src/main/java/com/example/dell/mysql/Constants.java
+++ app/src/main/java/com/example/dell/mysql/Constants.java
@@ -1,7 +1,7 @@
 package com.example.dell.mysql;
 
 public class Constants {
-    private static final String Root_URL="http://192.168.43.169/Android/v1/";
+    private static final String Root_URL="http://your pc ip/Android/v1/";
     public static final String URL_REGISTER = Root_URL+"registerUser.php";
     public static final String URL_LOGIN = Root_URL+"userLogin.php";
 
